92a4ed9f46a6da7c4d2d7bb9129647cc377cf413
aaroncapon <aaroncapon@cern.ch>
2017-08-28 15:52:43 +0200

    Rootlogon added. rootrc amended

---
+++ rootlogon.C
@@ -0,0 +1,45 @@
+//Root logon script
+{
+    std::cout << "New ROOT session. ALICE style plotting set." << std::endl;
+
+    gStyle->SetOptTitle(1);
+    gStyle->SetOptStat(0);
+    gStyle->SetPalette(56); //54
+    gStyle->SetCanvasColor(10);
+    gStyle->SetCanvasBorderMode(0);
+    gStyle->SetFrameLineWidth(1);
+    gStyle->SetFrameFillColor(kWhite);
+    gStyle->SetPadColor(10);
+    gStyle->SetPadTickX(1);
+    gStyle->SetPadTickY(1);
+    gStyle->SetPadBottomMargin(0.15);
+    gStyle->SetPadLeftMargin(0.15);
+    gStyle->SetHistLineWidth(1);
+    gStyle->SetHistLineColor(kRed);
+    gStyle->SetFuncWidth(2);
+    gStyle->SetFuncColor(kGreen);
+    gStyle->SetLineWidth(2);
+    gStyle->SetLabelSize(0.045,"xyz");
+    gStyle->SetLabelOffset(0.01,"y");
+    gStyle->SetLabelOffset(0.01,"x");
+    gStyle->SetLabelColor(kBlack,"xyz");
+    gStyle->SetTitleSize(0.05,"xyz");
+    gStyle->SetTitleOffset(1.4,"y");
+    gStyle->SetTitleOffset(1.2,"x");
+    gStyle->SetTitleFillColor(kWhite);
+    gStyle->SetTextSizePixels(26);
+    gStyle->SetTextFont(42);
+    //  gStyle->SetTickLength(0.04,"X");  gStyle->SetTickLength(0.04,"Y"); 
+
+    gStyle->SetLegendBorderSize(0);
+    gStyle->SetLegendFillColor(kWhite);
+    //  gStyle->SetFillColor(kWhite);
+    gStyle->SetLegendFont(42);
+
+
+    //Automatically create weights object for histograms
+    TH1::SetDefaultSumw2();
+    //Do not add histograms by name to TDirectory
+    TH1::AddDirectory(kFALSE);
+
+}
