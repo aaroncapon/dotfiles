b27117aa4e944834261607c22fa6c7a7461c0f17
aaroncapon <aaroncapon@cern.ch>
2018-11-30 13:13:24 +0100

    Adds tigrc and updates symlink script

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc tmux.conf vim latexmkrc inputrc"    # list of files/folders to symlink in homedir
+files="bashrc vimrc tmux.conf vim latexmkrc inputrc gdbinit tigrc"    # list of files/folders to symlink in homedir
 
 ##########
 
